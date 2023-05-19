17e41dc598b07f6001c6f9e8e623899dfd5bb809
Dan <35927536+dan-mba@users.noreply.github.com>
2023-05-19 13:14:44 -0400

    increase slice

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ require('dotenv').config()
 const mongo = process.env.LINKFREE_MONGO_CONNECTION_STRING;
 
 if (mongo[0] === '*') console.log('hidden');
-console.log(`Secret:${mongo.slice(0,15)}`);
+console.log(`Secret:${mongo.slice(0,25)}`);
